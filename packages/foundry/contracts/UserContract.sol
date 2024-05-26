efe8be918016e43bdd56ca0375bc7446cf0e99b8
shuanglong1104 <shuanglong1104@localhost>
2024-05-26 10:14:52 +0800

    update contracts

--- packages/foundry/contracts/UserContract.sol
+++ packages/foundry/contracts/UserContract.sol
@@ -5,12 +5,14 @@ pragma solidity ^0.8.20;
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./IUserContract.sol";
 
-abstract contract UserContract is IUserContract {
+contract UserContract is IUserContract {
     uint256 public gasUsedInUsdc;
     address admin;
+
     constructor() {
         admin = msg.sender;
     }
@@ -22,5 +24,41 @@ abstract contract UserContract is IUserContract {
 
     function accumulateGasInUsdc(uint256 _gasInUsdc) external onlyAdmin {
         gasUsedInUsdc += _gasInUsdc;
+    }
+
+    function transferETH(address to, uint256 amount) external payable override {
+        payable(to).transfer(amount);
+    }
+
+    function transferToken(
+        address token,
+        address to,
+        uint256 amount
+    ) external override {
+        IERC20(token).transfer(to, amount);
+    }
+
+    function TransferNFT(address token, address to) external override {
+        // transfer nft
+    }
+
+    function Approve(
+        address token,
+        address operator,
+        uint256 amount
+    ) external override {
+        // approve token
+    }
+
+    function ApproveNFT(
+        address nft,
+        address operator,
+        uint256 nftId
+    ) external override {
+        // approve nft
+    }
+
+    function ApproveAllNFT(address nft, address operator) external override {
+        // approve all nft
     }
 }

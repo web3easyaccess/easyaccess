70d602fce9dd2814d297ca11c99d2606743f3403
zhtkeepup <zhtkeepup@gmail.com>
2024-05-26 18:37:54 +0800

    xxxxx

--- packages/express-server/src/index.ts
+++ packages/express-server/src/index.ts
@@ -45,6 +45,7 @@ app.post("/permitUser", (req, res) => {
           permitUser(`0x${eoa}`, BigInt(nonce), `0x${signature}`).then(
             (info) => {
               try {
+                console.log("infoxxxxx:", info);
                 res.send(JSON.stringify(info));
               } catch (e) {
                 console.log("info:", info);
